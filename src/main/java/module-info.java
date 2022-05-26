47212a6bd1f4fada446009d09d93aadbe99e7eae
AlexixRugis <75169888+AlexixRugis@users.noreply.github.com>
2022-05-26 15:37:31 +0300

    initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.artech.timetableapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.artech.timetableapp to javafx.fxml;
+    exports com.artech.timetableapp;
+}
